ae283b0c5c6fcd43d2a8ec6865f96347725fcdab
Ngoc My <ps1403028@gmail.com>
2026-01-12 22:27:57 +0700

    feat: add ex1

---
+++ tests/lesson-2/ex1.js
@@ -0,0 +1,15 @@
+// a. Khai báo một hằng số number với giá trị là 12. 
+let number = 12;
+console.log(number);
+// b. Khai báo một biến name với giá trị là ”my number”
+const name = "my number";
+console.log(name);
+// c. Khai báo một biến isEven với giá trị là sai.
+let isEvent = false;
+console.log(isEvent);
+// d. Kiểm tra nếu number là số chẵn, gán lại isEven thành giá trị đúng. 
+if (number % 2 === 0) {
+    isEvent = true;
+    console.log(isEvent);
+}
+
