db4089b50ccd79fcead174c82d170bf702abda0d
Ngoc My <ps1403028@gmail.com>
2026-01-12 23:03:50 +0700

    Feat: add ex3 and ex3-advanced

---
+++ tests/lesson-2/ex3-advanced.js
@@ -0,0 +1,9 @@
+//  advanced
+const Number = -5;
+if (Number > 0) {
+    console.log("Giá trị bạn nhập là số dương");
+} else if (Number < 0) {
+    console.log("Giá trị bạn nhập là số âm");
+} else {
+    console.log("Giá trị bạn nhập là số 0");
+}
---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,22 @@
+/* 3. Tạo file ex3.js, thêm vào code đáp án cho đề bài sau: 
+a. Khai báo một hằng số với giá trị kiểu Number bất kỳ. 
+b. In ra dòng sau ở console: 
+*/
+const Number = -5;
+// i. Nếu hằng số lớn hơn 0: “Giá trị bạn nhập là số dương” 
+if (Number > 0) {
+    console.log("Giá trị bạn nhập là số dương");
+
+}
+// ii. Nếu hằng số nhỏ hơn 0: “Giá trị bạn nhập là số âm” 
+if (Number < 0) {
+    console.log("Giá trị bạn nhập là số âm");
+
+}
+// iii. Nếu hằng số = 0: “Giá trị bạn nhập là số 0”  
+
+if (Number === 0) {
+    console.log("Giá trị bạn nhập là số 0");
+
+}
+
